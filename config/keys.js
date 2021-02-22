8b13b1812fd7b0b44b04577f94ad5a0f7b3bd9ff
droid4alex <droid4alex@gmail.com>
2021-02-22 01:15:13 -0800

    Updates config keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://dev:101101@cluster0.hdid5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
