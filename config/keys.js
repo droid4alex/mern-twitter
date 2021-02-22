a0d2056ad4a5540ead59184a0c19004db9d4be06
droid4alex <droid4alex@gmail.com>
2021-02-22 01:11:47 -0800

    Add user-auth, tweets, users, components

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:101101@cluster0.hdid5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+}
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2021 Mern-Twitter
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
